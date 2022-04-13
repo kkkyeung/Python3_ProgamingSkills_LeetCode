352b15405999b9f5b59fd5bfb233e565d8151f70
Kin Kui Yeung <kensonyeung9524@gmail.com>
2022-04-13 23:11:07 +1000

    Day9 Part1 + 2

--- Day8/Part3/389.py
+++ Day8/Part3/389.py
@@ -3,8 +3,8 @@ class Solution:
         s = ''.join(sorted(s))
         t = ''.join(sorted(t))
 
-        print(s)
-        print(t)
+        #print(s)
+        #print(t)
         
         if len(s) == 0:
             return t
@@ -13,3 +13,15 @@ class Solution:
                 if s[i] != t[i]:
                     return t[i]
             return t[len(t) - 1]
+
+# Fast Solution in Discuss:
+# def findTheDifference(self, s, t):
+#     """
+#     :type s: str
+#     :type t: str
+#     :rtype: str
+#     """
+#     ans = 0
+#     for c in s + t:
+#         ans ^= ord(c)
+#     return chr(ans)
