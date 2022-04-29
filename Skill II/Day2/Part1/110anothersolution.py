f86f3ce50282bc342a1c627b36c9cc4fe498b132
Kin Kui Yeung <kensonyeung9524@gmail.com>
2022-04-29 21:00:14 +1000

    Skill II Day2 Update plus Day1Part2 SlicingmSolution

---
+++ Skill II/Day2/Part1/110anothersolution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.help(root) != -1
+        
+    def help(self, root):
+        if not root:
+            return 0
+        left = self.help(root.left)
+        if left == -1:  
+            return -1
+        right = self.help(root.right)
+        if right == -1: 
+            return -1
+        
+        if abs(left - right) > 1:
+            return -1
+        return max(left, right) + 1
