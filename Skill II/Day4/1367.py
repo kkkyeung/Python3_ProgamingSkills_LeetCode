21ad000530a5d3b1582d19714b949afea5f3be92
Kin Kui Yeung <kensonyeung9524@gmail.com>
2022-05-03 20:40:00 +1000

    Skill II Day4

---
+++ Skill II/Day4/1367.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubPath(self, head, root):
+        def dfs(head, root):
+            if not head: return True
+            if not root: return False
+            return root.val == head.val and (dfs(head.next, root.left) or dfs(head.next, root.right))
+        if not head: return True
+        if not root: return False
+        return dfs(head, root) or self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+
+#brute force
