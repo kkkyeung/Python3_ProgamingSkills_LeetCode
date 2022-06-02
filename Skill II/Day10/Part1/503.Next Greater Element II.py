8e71580957daa0008201b53b871442bc6ece32a5
Kin Kui Yeung <kensonyeung9524@gmail.com>
2022-06-02 23:22:51 +1000

    SK II Day10 Part1 Unfinished, will do tmr

---
+++ Skill II/Day10/Part1/503.Next Greater Element II.py
@@ -0,0 +1,30 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        output = []
+        for i in range(len(nums)-1):
+            if nums[i+1] > nums[i]:
+                output.append(nums[i+1])
+                continue
+            elif nums[i+1] == nums[i]:
+                newNumList = nums[i:]
+                for j in range(len(newNumList)):
+                    if not newNumList[j] > nums[i]:
+                        continue
+                    elif j == len(newNumList) -1 and not newNumList[j] > nums[i]:
+                        output.append(-1)
+                    else:
+                        output.append(newNumList[j])
+                        break
+            else:
+                output.append(-1)
+                
+                
+        if nums[len(nums)-1] > nums[len(nums)-2]:
+            output.append(-1)
+        else:
+            output.append(nums[len(nums)-2])
+        
+        # print(output)
+        return output
+
+# First Part Unfinished... So tired today :(    let do the remain part tomorrow
